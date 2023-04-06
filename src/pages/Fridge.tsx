cccee6c2e9035a557444a5bb898e96627d96d2e7
Richard Ngo <richard1ngo@gmail.com>
2023-04-06 02:59:18 -0400

    add ingrdient to fridge done

--- src/pages/Fridge.tsx
+++ src/pages/Fridge.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { db } from "../config/firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 import FridgeProps from '../interfaces/page';
+import KitchenIcon from "@mui/icons-material/Kitchen";
+import {
+    TextInput,
+    TextInputProps,
+    ActionIcon,
+    Button,
+    Group,
+    useMantineTheme,
+  } from "@mantine/core";
 
 
  
@@ -12,27 +21,41 @@ const Fridge: React.FC<FridgeProps> = (props) => {
 
       // Firestore API single get
   const userFridgeRef = collection(db, "users", user.uid, "fridge");
-  const [firestoreData, setFirestoreData] = React.useState("");
+  const [firestoreData, setFirestoreData] = React.useState([]);
   
   React.useEffect(() => {
-  async function fetchData() {
-    const querySnapshot = await getDocs(userFridgeRef);
-    console.log(querySnapshot);
-    
-    querySnapshot.docs.forEach((doc) => {
-      console.log(doc.data());
-      
-      setFirestoreData(JSON.stringify(doc.data()));
-    });
-  }
-  fetchData()
-}, []);
+    async function fetchData() {
+      const querySnapshot = await getDocs(userFridgeRef);
+      console.log(querySnapshot);
+      let dbData = [];
+      querySnapshot.docs.forEach((doc) => {
+        console.log(dbData);
+        dbData.push(doc.data());
+        setFirestoreData(dbData);
+      });
+    }
+    fetchData();
+  }, []);
 
 
   console.log(firestoreData);
     return ( 
         <>
             <h1>My Fridge</h1>
+            {firestoreData.map((item) => {
+            return (
+                <>
+                <p>{item.name}</p>
+                <img src={`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`}/>
+                <Group>
+                    <Button leftIcon={<KitchenIcon/>}></Button>
+                    <Button>-</Button>
+                    <Button>+</Button>
+                </Group>
+                </>
+            )
+        })
+        }
         </>
      );
 }
--- src/pages/Ingredients.tsx
+++ src/pages/Ingredients.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { db } from "../config/firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, setDoc, doc } from "firebase/firestore";
 import IngredientsProps from "../interfaces/page";
 import { useParams } from "react-router-dom";
 import KitchenIcon from "@mui/icons-material/Kitchen";
@@ -17,7 +17,8 @@ import { IconSearch, IconArrowRight, IconArrowLeft } from "@tabler/icons-react";
 const Ingredients: React.FC<IngredientsProps> = (props) => {
   const API_KEY = import.meta.env.VITE_SPOON_API_KEY;
 
-  const [firestoreData, setFirestoreData] = React.useState("");
+  const [firestoreData, setFirestoreData] = React.useState([]);
+  const [fridgeAdd, setFridgeAdd] = React.useState(false);
   const [searchValue, setSearchValue] = React.useState("");
   const [searchQuery, setSearchQuery] = React.useState("");
   const [searchData, setSearchData] = React.useState([]);
@@ -46,19 +47,37 @@ const Ingredients: React.FC<IngredientsProps> = (props) => {
         setSearchData(json.results)
   };
 
+  const addFridge = async (name, image, id) => {
+    console.log(name);
+    await setDoc(doc(userFridgeRef), { name, image, id });
+    fridgeAdd ? setFridgeAdd(false) : setFridgeAdd(true);
+};
+
   React.useEffect(() => {
     async function fetchData() {
       const querySnapshot = await getDocs(userFridgeRef);
       console.log(querySnapshot);
-
+      let dbData = [];
       querySnapshot.docs.forEach((doc) => {
-        console.log(doc.data());
-
-        setFirestoreData(JSON.stringify(doc.data()));
+        console.log(dbData);
+        dbData.push(doc.data());
+        setFirestoreData(dbData);
       });
     }
     fetchData();
-  }, []);
+
+  }, [fridgeAdd]);
+
+//   React.useEffect(() => {
+//     async function createData() {
+//       const querySnapshot = await setDoc(doc(userFridgeRef), {
+
+//       });
+//       console.log(querySnapshot);
+//       let dbData = [];
+//     }
+//     createData();
+//   });
 
   React.useEffect(() => {
     getIngredients();
@@ -94,22 +113,44 @@ const Ingredients: React.FC<IngredientsProps> = (props) => {
       />
 
         <h2>My Fridge</h2>
-        {firestoreData
+        {firestoreData.map((item) => {
+            return (
+              <>
+                <p>{item.name}</p>
+                <img
+                  src={`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`}
+                />
+                <Group>
+                  <Button
+                    onClick={addFridge}
+                    leftIcon={<KitchenIcon />}
+                  ></Button>
+                  <Button>-</Button>
+                  <Button>+</Button>
+                </Group>
+              </>
+            );
+        })
         }
 
         <h2>Search Results</h2>
         {searchData.map((item) => {
             return (
-                <>
+              <>
                 <p>{item.name}</p>
-                <img src={`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`}/>
+                <img
+                  src={`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`}
+                />
                 <Group>
-                    <Button leftIcon={<KitchenIcon/>}></Button>
-                    <Button>-</Button>
-                    <Button>+</Button>
+                  <Button
+                    onClick={() => addFridge(item.name, item.image, item.id)}
+                    leftIcon={<KitchenIcon />}
+                  ></Button>
+                  <Button>-</Button>
+                  <Button>+</Button>
                 </Group>
-                </>
-            )
+              </>
+            );
         })
         }
 
