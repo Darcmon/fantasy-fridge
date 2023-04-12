dc54bc9034cb192526dafba2934a11245e9374cd
Richard Ngo <richard1ngo@gmail.com>
2023-04-12 13:51:19 -0400

    add footer

---
+++ src/components/FooterDetails/FooterDetails.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface FooterDetailsProps {
+    
+}
+ 
+const FooterDetails: React.FC<FooterDetailsProps> = () => {
+    return ( 
+        <div>
+            <p>Pixel Collective</p>
+        </div>
+     );
+}
+ 
+export default FooterDetails;
